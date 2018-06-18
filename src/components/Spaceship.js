a6d75d0ea19e879703d6d35295b5827fb1d448b2
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-18 14:15:24 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+Spaceship.defaultProps = {
+  speed: "slow"
+  hasRockets: false
+  colors: ["black", "red"]
+}
+
 export default class Spaceship extends React.Component {
   render() {
     return (
-      <div className="movie-card">
-        <img src="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg" alt="Mad Max: Fury Road" />
-        <h2>Mad Max: Fury Road</h2>
-        <small>Genres: Action, Adventure, Science Fiction, Thriller</small>
+      <div className="spaceship">
+        <h1>{this.props.name}</h1>
+        <p>{this.props.speed}</p>
+        <p>{this.props.hasRockets}</p>
+        <small>this.props.colors.join(", ")</small>
       </div>
     );
   }
